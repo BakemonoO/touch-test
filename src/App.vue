2148a3860aeb72632150ce75bc82f79b1535ff53
Николай <pervertmel@gmail.com>
2023-08-24 00:57:27 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <transition name="modal">
+      <div
+        v-if="visible"
+        class="draggable-block"
+        ref="draggableBlock"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        :style="{ bottom: blockBottom + '%' }"
+      >
+        модалОчка
+      </div>
+    </transition>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      blockBottom: 0,
+      startTouchY: 0,
+      visible: true
+    };
+  },
+  methods: {
+    handleTouchStart(event) {
+      event.preventDefault();
+      this.startTouchY = event.touches[0].clientY;
+    },
+    handleTouchMove(event) {
+      const deltaY = event.touches[0].clientY - this.startTouchY;
+      const distanceInPercent = (deltaY / window.innerHeight) * 100;
+      if (distanceInPercent > 0) {
+        this.blockBottom -= distanceInPercent; // Изменили знак на минус
+        this.startTouchY = event.touches[0].clientY;
+      }
+    },
+    handleTouchEnd() {
+      if (this.blockBottom < 0) {
+        this.visible = false;
+      }
+      // Ваш код для обработки окончания перемещения на сенсорном устройстве
     }
   }
+};
+</script>
+
+<style scoped>
+.draggable-block {
+  position: absolute;
+  width: 100%;
+  height: 300px;
+  bottom: 10%;
+  background-color: #ccc;
+  cursor: grab;
+  transition: transform 0.1s ease; /* Добавляем плавную анимацию */
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: all 1s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  transform: translate(0, 100%);
 }
 </style>
